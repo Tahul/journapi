724547b2ab79b7132bc8aec98c2e9d8551ef9505
Yaël GUILLOUX <yael.guilloux@gmail.com>
2020-12-17 15:56:08 +0100

    :bug: (turbolinks) add turbolinks adapter as livewire2 removed built-in sup

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -115,6 +115,7 @@
     @yield('content')
 
     <livewire:scripts />
+    <script src="https://cdn.jsdelivr.net/gh/livewire/turbolinks@v0.1.x/dist/livewire-turbolinks.js" data-turbolinks-eval="false"></script>
 </div>
 </body>
 </html>
